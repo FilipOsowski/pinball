e4f776119267d4859e57da3e36d56f716c14568a
Filip Osowski <filiposowski5@gmail.com>
2018-12-05 20:43:27 -0600

    Fix balls not spawning, clean up code

--- pinball.py
+++ pinball.py
@@ -23,6 +23,7 @@ collision_types = {
 def flipyv(v):
     return int(v.x), int(-v.y + 600)
 
+
 def add_bumper(space, location, radius):
     body = pymunk.Body(body_type=pymunk.Body.STATIC)
 
@@ -32,6 +33,8 @@ def add_bumper(space, location, radius):
     shape.collision_type = collision_types["bumper"]
 
     space.add(body, shape)
+
+
 def add_bumper_collision_handler(space):
     ch = space.add_collision_handler(collision_types["ball"], collision_types["bumper"])
 
@@ -53,29 +56,32 @@ def add_bumper_collision_handler(space):
 
     ch.post_solve = post_solve
 
-def add_powerup(space,color,position): #adds circular power ups that affect ball differently upon impact
+
+def add_powerup(space,color,position):  # adds circular power ups that affect ball differently upon impact
     pow=pymunk.Circle(space.static_body,15)
     pow.body.position= position
     pow.collision_type = collision_types["powerup"]
     pow.color = color
     space.add(pow)
-def add_powerup_collision_handler(space):#collision between ball and powerup
+
+
+def add_powerup_collision_handler(space):  # collision between ball and powerup
     def remove_pow(arbiter, space, data):
         circ= arbiter.shapes[0]
         ball = arbiter.shapes[1]
-        if (circ.color == THECOLORS["blue"]):#makes ball go faster
+        if (circ.color == THECOLORS["blue"]):  # makes ball go faster
             print("fast")
             space.remove(ball.body,ball)
             spawn_ball(space,ball.body.position,ball.body.velocity*2)
             #ball.body.velocity*=2
-        elif (circ.color == THECOLORS["red"]):#adds new ball in screen and makes ball go faster
+        elif (circ.color == THECOLORS["red"]):  # adds new ball in screen and makes ball go faster
             print("both")
            # ball.body.velocity/=5
             spawn_ball(space, (random.randint(50, 550), 500), (random.randint(-100, 100), random.randint(-100, 100)))
             space.remove(ball.body, ball)
             spawn_ball(space, ball.body.position, ball.body.velocity * 2)
             #ball.body.velocity*=3
-        else:#adds new ball in screen
+        else:  # adds new ball in screen
             print("new")
             spawn_ball(space, (random.randint(50, 550), 500), (random.randint(-100, 100), random.randint(-100, 100)))
         print(ball.body.velocity)
@@ -83,54 +89,59 @@ def add_powerup_collision_handler(space):#collision between ball and powerup
     h = space.add_collision_handler(collision_types["powerup"],collision_types["ball"])
     h.separate = remove_pow
 
-def add_transport(space,posStart,posEnd,posStart2,posEnd2): #adds segments that transport ball across the layout
-    trans = pymunk.Segment(space.static_body, posStart, posEnd, 5)     #segment on left
+
+def add_transport(space, posStart, posEnd, posStart2, posEnd2):  # adds segments that transport ball across the layout
+    trans = pymunk.Segment(space.static_body, posStart, posEnd, 5)  # segment on left
     trans.body.position.x = posStart[0]
     trans.collision_type = collision_types["trans1"]
     trans.color = THECOLORS["green"]
 
-    trans2 = pymunk.Segment(space.static_body, posStart2, posEnd2, 5)     #segment on right
+    trans2 = pymunk.Segment(space.static_body, posStart2, posEnd2, 5)  # segment on right
     trans2.body.position.x = posStart2[0]
     trans2.collision_type = collision_types["trans2"]
     trans2.color = THECOLORS["green"]
-    space.add(trans,trans2)
+    space.add(trans, trans2)
 
-    def move_ball_left(arbiter, space, data):    #changes ball's position to the left, after collision with the right segment
+    def move_ball_left(arbiter, space, data):  # changes ball's position to the left, after collision with the right segment
         print("left")
         ball = arbiter.shapes[0]
-        space.remove(ball.body, ball)#removes ball from space
-        spawn_ball(space, (trans.body.position.x + 5, ball.body.position.y),ball.body.velocity * -1)#spawns ball in again with new velocity and in the left position
-    def move_ball_right(arbiter, space, data):  #changes ball's position to the right, after collision with the left segment
+        space.remove(ball.body, ball)  # removes ball from space
+        spawn_ball(space, (trans.body.position.x + 5, ball.body.position.y),ball.body.velocity * -1)  # spawns ball in again with new velocity and in the left position
+
+    def move_ball_right(arbiter, space, data):  # changes ball's position to the right, after collision with the left segment
         print("right")
         ball = arbiter.shapes[0]
         space.remove(ball.body,ball)
         spawn_ball(space,(trans.body.position.x + (posStart2[0] - abs(posStart[0])) - 5, ball.body.position.y),ball.body.velocity*-1)#spawns ball in again with new velocity and in the right position
-    h = space.add_collision_handler(#adds collision betweel ball and left transport
+    h = space.add_collision_handler(  # adds collision between ball and left transport
         collision_types["ball"],
         collision_types["trans1"])
     h.separate = move_ball_right
-    h2 = space.add_collision_handler(#adds collision betweel ball and right transport
+    h2 = space.add_collision_handler(  # adds collision between ball and right transport
         collision_types["ball"],
         collision_types["trans2"])
     h2.separate = move_ball_left
-    return trans,trans2
-def gen_rail(space,pInit,pFin):#generates series of short line segments to give impression of a curve
+    return trans, trans2
+
+
+def gen_rail(space, pInit, pFin):  # generates series of short line segments to give impression of a curve
     rail = []
     distX = (pFin[0]-pInit[0])
-    distY=(pFin[1]-pInit[1])
-    pX= pInit[0]
-    pY= pInit[1]
+    distY = (pFin[1]-pInit[1])
+    pX = pInit[0]
+    pY = pInit[1]
     tot = 23
     for num in range(tot):
-        rail.append(pymunk.Segment(space.static_body, (pX,pY),(pX+(distX/tot),pY+(distY/tot)+((tot/2)-num)*3),6))
-        pX+=distX/tot
-        pY+=distY/tot
-        pY+=((tot/2)-num)*3
+        rail.append(pymunk.Segment(space.static_body, (pX, pY), (pX+(distX/tot), pY+(distY/tot)+((tot/2)-num)*3), 6))
+        pX += distX/tot
+        pY += distY/tot
+        pY += ((tot/2)-num)*3
     for line in rail:
         line.color = THECOLORS['lightgray']
         line.elasticity = 0.21
         space.add(line)
     return rail
+
 
 def spawn_ball(space, position, direction):
     ball_body = pymunk.Body(1, pymunk.inf)
@@ -161,12 +172,14 @@ def setup_level(space):
     add_bumper(space, (200, 300), 20)  # Add a bumper with position and radius
     add_bumper(space, (400, 300), 20)
 
-    add_powerup_collision_handler(space)   #adds power up obstacles that change ball
+    add_powerup_collision_handler(space)  # adds power up obstacles that change ball
     add_powerup(space,THECOLORS["red"],(300,400))
     add_powerup(space, THECOLORS["blue"], (475, 350))
     add_powerup(space, THECOLORS["yellow"], (100, 150))
 
-    add_transport(space,(-50,50),(-50,100),(450,50),(450,100))      #adds transport segments that move ball
+    add_transport(space,(-50,50),(-50,100),(450,50),(450,100))  # adds transport segments that move ball
+
+
 def add_out_of_bounds_collision_handler(space):
     def begin(arbiter, space, data):
         ball_shape = arbiter.shapes[0]
@@ -178,7 +191,10 @@ def add_out_of_bounds_collision_handler(space):
         collision_types["out_of_bounds"])
     h.begin = begin
 
+
 spring_segment_body = None
+
+
 def add_spring(space):
 
     spring_anchor_body = pymunk.Body(body_type = pymunk.Body.STATIC)
@@ -188,7 +204,6 @@ def add_spring(space):
     body.position = (580, 100)
     l1 = pymunk.Segment(body, (-18.6, 0), (18.6, 0), 20)
 
-#615,350
     rest_length = 300
     rest_length = 100
     stiffness = 3000
@@ -201,7 +216,6 @@ def add_spring(space):
 
 def add_boundaries(space):
     static_lines = [pymunk.Segment(space.static_body, (50, 100), (50, 700), 4),
-                    #pymunk.Segment(space.static_body, (50, 850), (630, 850), 4),
                     pymunk.Segment(space.static_body, (565, 650), (565, 50), 4),
                     pymunk.Segment(space.static_body, (50, 100), (225, 50), 4),
                     pymunk.Segment(space.static_body, (550, 100), (375, 50), 4),
@@ -209,7 +223,6 @@ def add_boundaries(space):
                     pymunk.Segment(space.static_body, (550, 450), (565, 450), 4),
                     pymunk.Segment(space.static_body, (615, 678), (615, 50), 4),
                     pymunk.Segment(space.static_body, (630, 678), (630, 50), 4),
-                    #pymunk.Segment(space.static_body, (550, 850), (615, 700), 4),
                     ]
     gen_rail(space, (50, 700), (633, 678))
     for line in static_lines:
@@ -224,12 +237,12 @@ def add_boundaries(space):
         line.collision_type = collision_types["out_of_bounds"]
         line.color = THECOLORS["red"]
 
-
     static_lines.append(out_of_bounds_area)
     space.add(static_lines)
 
 
 spring_anchor = None
+
 
 def main():
     # PyGame init
@@ -245,11 +258,11 @@ def main():
     draw_options = pymunk.pygame_util.DrawOptions(screen)
     spring = add_spring(space)
     global state
+
     # Start game
     setup_level(space)
 
     pointer_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-    # pointer_body.angle= 7*math.pi/4
     pointer_body2 = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 
     ps = [(10, 0), (0, 0), (0, 60), (10, 60)]
@@ -276,7 +289,6 @@ def main():
     space.add(gun_body, gun_shape, rotary_spring)
     space.add(gun_body2, gun_shape2, rotary_spring2)
 
-
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -285,6 +297,10 @@ def main():
                 running = False
             elif event.type == KEYDOWN and event.key == K_SPACE:
                 spring.rest_length = 10
+            elif event.type == KEYUP and event.key == K_SPACE:
+                spawn_ball(space, (590, 200), (0, 0))
+                spring.rest_length = 150
+
             if event.type == KEYDOWN and event.key == K_a:
                 rotary_spring.rest_angle = math.pi / 4
             if event.type == KEYUP and event.key == K_a:
@@ -293,27 +309,6 @@ def main():
                 rotary_spring2.rest_angle = -5 * math.pi / 4
             if event.type == KEYUP and event.key == K_d:
                 rotary_spring2.rest_angle = - 7 * math.pi / 4
-
-                def normalize_vector(a, b):
-                    v = [b[0] - a[0], b[1] - a[1]]
-                    r = (v[0] ** 2 + v[1] ** 2) ** (1 / 2)
-                    v = [v[0] / r, v[1] / r]
-                    return v
-
-                    # pos = flipyv(Vec2d(event.pos))
-
-                direction = normalize_vector((580, 110), (580, 110))
-                strength = 5
-                print("DIRECTION IS")
-                print(direction)
-                direction = map(lambda x: x * strength, direction)
-                spawn_ball(space, (590, 200), direction)
-
-            elif event.type == KEYUP and event.key == K_SPACE:
-
-                spring.rest_length = 150
-            # elif event.type == pygame.MOUSEBUTTONUP:
-
 
         # Clear screen
         screen.fill(THECOLORS["black"])
@@ -339,5 +334,4 @@ def main():
 
 
 if __name__ == '__main__':
-    yval = 100
     sys.exit(main())
